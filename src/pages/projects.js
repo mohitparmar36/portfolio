ff363dcd54a294745e7b1d1f734e0f15a3d54914
mohit.parmar36 <mohit.parmar2022@gmail.com>
2024-06-15 12:03:11 +0530

    mp1

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -92,9 +92,9 @@ const projects = () => {
                         <FeaturedProject 
                         title={"Doctor Appointment Web Application"}
                         img={projectImage}
-                        github={"/"}
+                        github={"https://github.com/mohitparmar36/MediConnect-Doctor-Web-App"}
                         summary={"A feature-rich Doctor Appointment Application using React, NodeJs, ExpressJs, MongoDb, Tailwind CSS, Context API, React Router and POSTMAN.It allows users to effortlessly book and manage their doctor appointments. You can easily view and select available appointment slots in real-time."}
-                        link={"/"}
+                        link={"https://github.com/mohitparmar36/MediConnect-Doctor-Web-App"}
                         type={"Featured Project"}
 
                         
